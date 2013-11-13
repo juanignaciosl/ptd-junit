dc81dcddc2af94ffa863ce9f224d66fb5b4edb64
Juan Ignacio Sánchez Lara <juanignaciosl@gmail.com>
2013-11-13 22:15:02 +0100

    DRY

--- test/com/juanignaciosl/ptd/junit/PtdDatesTest.java
+++ test/com/juanignaciosl/ptd/junit/PtdDatesTest.java
@@ -1,64 +1,58 @@
 package com.juanignaciosl.ptd.junit;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
-import junit.framework.Assert;
-
+import org.junit.Before;
 import org.junit.Test;
 
 public class PtdDatesTest {
+	private static final SimpleDateFormat SDF = new SimpleDateFormat(
+			"dd/MM/yyyy");
+
+	private PtdDate january1;
+	private PtdDate january2;
+
+	@Before
+	public void setUp() throws ParseException {
+		january1 = new PtdDate(SDF.parse("01/01/2013"));
+		january2 = new PtdDate(SDF.parse("02/01/2013"));
+	}
 
 	@Test
 	public void theDayAfterJanuary1IsJanuary2() throws ParseException {
-		final SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-		final PtdDate january1 = new PtdDate(sdf.parse("01/01/2013"));
-		final PtdDate january2 = new PtdDate(sdf.parse("02/01/2013"));
-
-		Assert.assertEquals(january2, january1.nextDay());
+		assertEquals(january2, january1.nextDay());
 	}
 
 	@Test
 	public void theDayBeforeJanuary2IsJanuary1() throws ParseException {
-		final SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-		final PtdDate january1 = new PtdDate(sdf.parse("01/01/2013"));
-		final PtdDate january2 = new PtdDate(sdf.parse("02/01/2013"));
-
-		Assert.assertEquals(january1, january2.previousDay());
+		assertEquals(january1, january2.previousDay());
 	}
 
 	@Test
 	public void theDayBeforeTheDayAfterJanuary1IsJanuary1()
 			throws ParseException {
-		final SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-		final PtdDate january1 = new PtdDate(sdf.parse("01/01/2013"));
-
-		Assert.assertEquals(january1, january1.nextDay().previousDay());
+		assertEquals(january1, january1.nextDay().previousDay());
 	}
 
 	@Test
 	public void theDayAfterJanuary1IsAfterIt() throws ParseException {
-		final SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-		final PtdDate january1 = new PtdDate(sdf.parse("01/01/2013"));
-
-		Assert.assertTrue(january1.nextDay().after(january1));
+		assertTrue(january1.nextDay().after(january1));
 	}
-	
+
 	@Test
 	public void twoEqualDatesAreNotTheSameObject() throws ParseException {
-		final SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-		final PtdDate january1 = new PtdDate(sdf.parse("01/01/2013"));
-		final PtdDate otherJanuary1 = new PtdDate(sdf.parse("01/01/2013"));
-
-		Assert.assertEquals(january1, otherJanuary1);
+		final PtdDate otherJanuary1 = new PtdDate(SDF.parse("01/01/2013"));
+		assertEquals(january1, otherJanuary1);
 	}
-	
+
 	@Test
 	public void canRetrieveJanuary1Epoch() throws ParseException {
-		final SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-		final PtdDate january1 = new PtdDate(sdf.parse("01/01/2013"));
-
-		Assert.assertNotNull(january1.getEpoch());
+		assertNotNull(january1.getEpoch());
 	}
 
 }
